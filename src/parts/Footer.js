089ead1d9ad674219bfc30555dbc6664b9b0806f
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-10-10 22:34:15 +0700

    change: footer style

--- src/parts/Footer.js
+++ src/parts/Footer.js
@@ -5,29 +5,29 @@ function Footer() {
       <div className="w-full flex flex-col lg:flex-row text-grey px-5 pb-10 md:px-20 lg:gap-16">
         <div className="basis-1 lg:basis-4/6">
           <div className="flex flex-row justify-items-center">
-            <img src="/images/logo-tedi.svg" alt="logo tedi" className="mr-5 w-2/5" />
-            <h2 className="text-lg text-white  lg:text-2xl text-justify">TeDi (Teman Disabilitas) adalah startup sosial yang bergerak dibidang penyediaan aplikasi aksesibilitas dengan teknologi <span className="italic">machine learning.</span></h2>
+            <img src="/images/title-logo.svg" alt="logo tedi" className="mr-5 w-2/5" />
+            <h2 className="text-lg text-white  lg:text-1xl text-justify">I'm willing to do anything good for people. Highly motivated to learn something new. I always keep telling myself to become the best version of myself anywhere and any time, to focus up, to push myself at the highest limit, and keep moving forward no matter what happens, because you only live once, YOLO!</h2>
           </div>
-          <div className="text-white flex flex-col lg:flex-row flex-wrap items-center gap-4 py-10">
+          {/* <div className="text-white flex flex-col lg:flex-row flex-wrap items-center gap-4 py-10">
             <h4>Didukung oleh</h4>
             <div className="flex flex-row gap-x-4 items-center">
               <img src="./images/logo-kemendikbud.png" alt="logo kemendikbud" className="h-full" />
               <img src="./images/logo-google.png" alt="logo kemendikbud" className="h-full" />
               <img src="./images/logo-bangkit.png" alt="logo kemendikbud" className="h-full" />
             </div>
-          </div>
+          </div> */}
         </div>
         <div className="text-white basis-1 lg:basis-2/6">
           <h2 className="text-xl lg:text-2xl font-bold mb-5">Kontak</h2>
           <ul className="">
             <li className="mb-3 flex">
-              <img src="./images/ic-loc.svg" alt="icon" className="w-5 mr-3"/><a href='https://goo.gl/maps/yaGzumzDsrcUjJAX6'>Fakultas Matematika dan Ilmu Pengetahuan Alam, Program Studi Sarjana Statistika, Universitas Padjadjaran.</a>
+              <img src="./images/ic-loc.svg" alt="icon" className="w-5 mr-3"/><a href='https://goo.gl/maps/yaGzumzDsrcUjJAX6'>Gg. Garboruci, Kledokan, Caturtunggal, Kec. Depok, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55281</a>
             </li>
             <li className="mb-3 flex">
-              <img src="./images/ic-mail.svg" alt="icon" className="w-5 mr-3"/><a href="mailto:info@temandisabilitas.com">info@temandisabilitas.com</a>
+              <img src="./images/ic-mail.svg" alt="icon" className="w-5 mr-3"/><a href="mailto:hazlanqodri2020work@gmail.com">hazlanqodri2020work@gmail.com</a>
             </li>
             <li className="mb-3 flex">
-              <img src="./images/ic-ig.svg" alt="icon" className="w-5 mr-3"/><a href="https://instagram.com/tedi.temandisabilitas">@tedi.temandisabilitas</a>
+              <img src="./images/ic-ig.svg" alt="icon" className="w-5 mr-3"/><a href="https://instagram.com/hzln.qodrey">@hzln.qodrey</a>
             </li>
           </ul>
         </div>
